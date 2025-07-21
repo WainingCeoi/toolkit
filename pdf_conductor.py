138f75f54649a4586cd07bec88fba37ca1298ddd
WeiningXu <weiningxu1018@gmail.com>
2025-07-21 19:02:46 +0800

    Conduct all IMG to PDF in Sequences

---
+++ pdf_conductor.py
@@ -0,0 +1,37 @@
+import os
+from PIL import Image
+from ToolFunc.colloctor import collect_target_files
+
+# Config parameters
+input_path = r"/Users/xuweining/Desktop/raw"
+output_path = r"/Users/xuweining/Desktop"
+extention = ['.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff']
+
+# Collect all image files
+image_files = collect_target_files(input_path, ext=extention, include_subfolder=False)
+
+
+if image_files:
+    print(f"Found {len(image_files)} Page.")
+else:
+    print("No images found in this folder.")
+    exit()
+
+# Sort image files by name
+image_files.sort()
+
+# Convert to PDF
+images = []
+for img_path in image_files:
+    img = Image.open(img_path).convert('RGB')
+    images.append(img)
+
+
+# Prompt user for output PDF file name
+# pdf_name = f"{input("Enter the output PDF file name: ")}.pdf"
+pdf_name = r".pdf"
+output_pdf = os.path.join(output_path, pdf_name)
+
+# Save PDF
+images[0].save(output_pdf, save_all=True, append_images=images[1:])
+print("Done!")
