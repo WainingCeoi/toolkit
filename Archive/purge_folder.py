9f07ac213c7e239be977de70759a7d1f5270158f
Weining <weiningxu1018@gmail.com>
2025-08-09 01:14:41 +0800

    Purge Specific Folder Cache

--- Archive/purge_folder.py
+++ Archive/purge_folder.py
@@ -2,27 +2,30 @@ import os
 from concurrent.futures import ThreadPoolExecutor
 from ToolFunc.colloctor import collect_target_files
 
+
 # Config parameters
 folder_to_purge = r""
 extensions = [".dwl", ".dwl2", ".bak", ".log", ".db", ".tmp", ".err"]
 min_workers = 200
 
-def delete_file(file):
+
+def delete_file(files):
     try:
-        os.remove(file)
+        for file in files:
+            os.remove(file)
     except Exception as e:
         print(e)
 
 
 if __name__ == "__main__":
     # get the target to delete files
-    files_to_delete = collect_target_files(input_folder=folder_to_purge, ext=extensions)
+    files_to_delete = collect_target_files(input_folder=folder_to_purge, file_types=extensions, include_subfolder=True)
     total_files = len(files_to_delete)
     print(f"Found {total_files} files.")
 
 
     dynamic_workers = max(min_workers, total_files)
     with ThreadPoolExecutor(max_workers=dynamic_workers) as executor:
-        futures = [executor.submit(delete_file, file) for file in files_to_delete]
+        executor.map(delete_file, (files_to_delete[idx::dynamic_workers] for idx in range(dynamic_workers)))
 
     print("Purge Done!")
