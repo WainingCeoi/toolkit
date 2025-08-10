0096f729dfc6f0dee73fce9f10b8e1aa8860df99
Weining <weiningxu1018@gmail.com>
2025-08-10 21:46:06 +0800

    Fetch PDF and Add its Bookmarks

--- Archive/fetch_pdf.py
+++ Archive/fetch_pdf.py
@@ -1,10 +1,9 @@
-from ToolFunc.fetchor import fetch_pdf, add_bookmark
+from ToolFunc.fetchor import fetch_pdf
 
 
-url = "https://ebook.chinabuilding.com.cn/zbooklib/bookpdf/probation?SiteID=1&bookID=59444"
-pdf_name = "/Users/xuweining/Desktop/1.pdf"
+url = ""
+output_path = ""
 
 
 if __name__ == "__main__":
-    fetch_pdf(url, pdf_name)
-    add_bookmark(url, pdf_name, pdf_name)
+    fetch_pdf(url, output_path)
