efd769b602273d0335cd2c0a97ef014959407fad
WeiningXu <weiningxu1018@gmail.com>
2026-01-11 21:08:21 +0800

    REMUX Processor with FFmpeg

--- REMUX_processor.py
+++ REMUX_processor.py
@@ -2,20 +2,20 @@ import ffmpeg
 from concurrent.futures import ProcessPoolExecutor
 from pathlib import Path
 import os
+from tkinter import filedialog
 
 
-def run_ffmpeg_task(video_in, sub_in, video_out, track_idx):
+def run_ffmpeg_task(input_video, input_subtitle, output_video, track_idx):
     # A pure worker function. It doesn't care about season or episode numbers, it just takes paths and executes them.
     try:
-        steam = ffmpeg.input(video_in)
+        steam = ffmpeg.input(input_video)
         video = steam[f"v:{track_idx["video"]}"]
         audio = steam[f"a:{track_idx["audio"]}"]
-        in_title = Path(video_in).stem
-        out_title = Path(video_out).stem
+        stream_title = Path(input_video).stem
 
         # Determine subtitle source
-        if sub_in:
-            subtitle = ffmpeg.input(sub_in).subtitle
+        if input_subtitle:
+            subtitle = ffmpeg.input(input_subtitle).subtitle
         else:
             subtitle = steam[f"s:{track_idx["subtitle"]}"]
 
@@ -27,57 +27,61 @@ def run_ffmpeg_task(video_in, sub_in, video_out, track_idx):
             "metadata:s:s:0": "language=chi",
             "metadata:s:s:0": "title=简英",
             "disposition:s:0": "default",
-            "metadata:g": f"title={out_title}"
+            "metadata:g": f"title={stream_title}"
         }
 
         (
             ffmpeg
-            .output(*streams, video_out, **out_config)
+            .output(*streams, output_video, **out_config)
             .overwrite_output()
             .run(capture_stdout=True, capture_stderr=True)
         )
 
-        return f"✅ Success: {out_title}"
+        return f"✅ Success: {stream_title}"
     except Exception as e:
-        return f"❌ Failed: {in_title} | Error: {e}"
+        return f"❌ Failed: {stream_title} | Error: {e}"
+
+
+def get_files():
+    file_paths = filedialog.askopenfilenames(
+        title = "Please select the raw video files",
+    )
+    return file_paths
 
 
 if __name__ == "__main__":
     # --- 1. Configuration ---
-    se = 1
-    ep_num = 10
-    in_path = "/Users/"
-    out_path = "/Users/"
+    out_path = "/Users"
     track_settings = {"video": 0, "audio": 0, "subtitle": 0}
     extra_sub = False
-    max_worker = 5
+
+
     # --- 2. Build the Task List ---
     # This is where you handle the varying filenames by manually defining different patterns for different episodes
     tasks = []
-    for ep in range(ep_num):
-        ep += 1
-        in_v_name = f"Samples.S{se:02}.E{ep:02}.mkv"
-        in_s_name = f""
-        out_file_name = f"Samples.se{se}.ep{ep}.mkv"
-        video_in = os.path.join(in_path, in_v_name)
-        sub_in = os.path.join(in_path, in_s_name)
-        video_out = os.path.join(out_path, out_file_name)
+    raw_video_files = get_files()
+    raw_subtitle_files = get_files() if extra_sub else None
+    tasks_num = len(raw_video_files)
+    assert tasks_num > 0, "❌ No file(s) selected."
 
-        if os.path.exists(video_in):
-            tasks.append({
-                "video_in": in_path + in_v_name,
-                "sub_in": sub_in if extra_sub else None,
-                "video_out": video_out,
-                "track_idx": track_settings
-            })
-        else:
-            print(f"❌{in_v_name} doesn't exist")
+    for idx in range(tasks_num):
+        in_video = raw_video_files[idx]
+        in_subtitle = raw_subtitle_files[idx] if extra_sub else None
+        streams_title = os.path.basename(in_video)
+        out_video = os.path.join(out_path, streams_title)
+
+        tasks.append({
+            "input_video": in_video,
+            "input_subtitle": in_subtitle,
+            "output_video": out_video,
+            "track_idx": track_settings,
+        })
 
 
     # --- 3. Execute Simultaneously ---
-    print(f"Starting parallel processing for {len(tasks)} files...")
+    print(f"Starting parallel processing for {tasks_num} files...")
 
-    with ProcessPoolExecutor(max_workers=max_worker) as executor:
+    with ProcessPoolExecutor(max_workers=tasks_num) as executor:
         # We use a dictionary unpacking (**) to pass the task info to the function
         futures = [executor.submit(run_ffmpeg_task, **task) for task in tasks]
 
