e567f48a3fc82bd86c58c159cbdd67dc49fb9019
WeiningXu <weiningxu1018@gmail.com>
2026-03-18 12:16:59 +0800

    Files migration utility

--- move_files.py
+++ move_files.py
@@ -7,9 +7,22 @@ target_folder = os.path.expanduser("~Desktop/2")
 
 v_types = [".mp4", ".mov", ".ts", ".flv"]
 files = collect_target_files(input_folder=source_folder, file_types=v_types, include_subfolder=True)
+print(f"Found {len(files)} files.")
 
 
 for file in files:
-    file_name = os.path.basename(file)
-    target_path = os.path.join(target_folder, file_name)
-    os.rename(file, target_path)
+    try:
+        file_name = os.path.basename(file)
+        name, ext = os.path.splitext(file_name)
+        target_path = os.path.join(target_folder, file_name)
+
+        # Module use for handling duplicated files.
+        counter = 1
+        while os.path.exists(target_path):
+            new_file_name = f"{name}_{counter}{ext}"
+            target_path = os.path.join(target_folder, new_file_name)
+            counter += 1
+
+        os.rename(file, target_path)
+    except Exception as e:
+        print(f"Error moving {file}: {e}")
