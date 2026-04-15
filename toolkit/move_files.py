360f1cdbf716af5ade8c0c14fc91d6f0c9e130b1
WeiningXu <weiningxu1018@gmail.com>
2026-04-15 14:58:33 +0800

    Updated files collecting engine

--- toolkit/move_files.py
+++ toolkit/move_files.py
@@ -1,17 +1,29 @@
-from toolkit.tool_function.colloctor import collect_target_files
 import os
 import subprocess
+from scandir_rs import Scandir
 
 
+# Configuration
 source_folder = os.path.expanduser("~/Desktop/1")
 target_folder = os.path.expanduser("~/Desktop/2")
-os.makedirs(target_folder, exist_ok=True)
+v_types = ["*.mkv", "*.mp4", "*.mov", "*.ts", "*.flv", "*.avi"]
 
-v_types = [".mp4", ".mov", ".ts", ".flv", ".avi"]
-files = collect_target_files(input_folder=source_folder, file_types=v_types, include_subfolder=True)
+# Collect targeted files path
+os.makedirs(target_folder, exist_ok=True)
+scaner, errors = Scandir(source_folder, file_include=v_types).collect()
+files = []
+for file in scaner:
+    if file.is_file:
+        files.append(os.path.join(source_folder, file.path))
+
+if errors:
+    for error in errors:
+        print(error)
+
 print(f"Found {len(files)} files.")
 
 
+# Move files
 for file in files:
     try:
         file_name = os.path.basename(file)
@@ -29,4 +41,5 @@ for file in files:
     except Exception as e:
         print(f"Error moving {file}: {e}")
 
+print("Done!")
 subprocess.run(["afplay", "/System/Library/Sounds/Hero.aiff"])
