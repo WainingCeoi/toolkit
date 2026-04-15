46664e3a029b221c2ea1bce941f131c9cf25909e
WeiningXu <weiningxu1018@gmail.com>
2026-04-15 19:26:13 +0800

    Bugs fixed

--- toolkit/pdf_conductor.py
+++ toolkit/pdf_conductor.py
@@ -2,33 +2,28 @@ import os
 from PIL import Image
 from tkinter.filedialog import askopenfilenames as get_files
 
-# Config parameters
-input_path = os.path.expanduser("~/Desktop/raw")
+
+# Config parameter
 output_path = os.path.expanduser("~/Desktop")
 
-# Collect all image files
-image_files = get_files(title="Please select image(s)")
-
-
+# Collect images
+image_files = sorted(get_files(title="Please select image(s)"))
 if image_files:
-    print(f"Found {len(image_files)} Page.")
+    print(f"Processing {len(image_files)} Page...")
 else:
-    print("No images found in this folder.")
+    print("❌ No image selected.")
     exit()
 
-# Sort image files by name
-image_files.sort()
 
 # Convert to PDF
 images = []
 for img_path in image_files:
-    img = Image.open(img_path).convert('RGB')
+    img = Image.open(img_path).convert("RGB")
     images.append(img)
 
 
-# Prompt user for output PDF file name
-# pdf_name = f"{input("Enter the output PDF file name: ")}.pdf"
-pdf_name = r".pdf"
+# Prompt for PDF name
+pdf_name = f"{input("Enter the output PDF file name: ")}.pdf"
 output_pdf = os.path.join(output_path, pdf_name)
 
 # Save PDF
