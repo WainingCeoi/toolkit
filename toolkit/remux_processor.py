b8863b4d175a537e66f7c4b38803214d192643be
WeiningXu <weiningxu1018@gmail.com>
2026-04-22 17:01:00 +0800

    Bug fixed

--- toolkit/remux_processor.py
+++ toolkit/remux_processor.py
@@ -79,7 +79,7 @@ if __name__ == "__main__":
     # --- 3. Execute Simultaneously ---
     print(f"Starting processing for {tasks_num} files...")
 
-    with ProcessPoolExecutor(max_workers=tasks_num) as executor:
+    with ProcessPoolExecutor() as executor:
         # We use a dictionary unpacking (**) to pass the task info to the function
         futures = [executor.submit(run_ffmpeg_task, **task) for task in tasks]
 
